a0e30066fac29d604ba38324a56a706aa893ea6f
xrey007 <reinaldo.leal001@gmail.com>
2017-11-20 21:33:09 -0500

    testing

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const ejs = require('ejs');
+const myFileLoad = function (filePath) {
+  return 'myFileLoad: ' + fs.readFileSync(filePath);
+};
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .set('read engine', 'ejs')
+  .get('/', (req, res) => ejs.fileLoader = myFileLoad('./bot.js'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
