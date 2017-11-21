02c07f1722fbe9473e8603f4e83da4e3de08234b
xrey007 <reinaldo.leal001@gmail.com>
2017-11-20 22:09:18 -0500

    using index again, and routing to bot

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 5000
-const ejs = require('ejs');
-const myFileLoad = function (filePath) {
-  return 'myFileLoad: ' + fs.readFileSync(filePath);
-};
+var bot = require('./bot.js');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('read engine', 'ejs')
-  .get('/', (req, res) => ejs.fileLoader = myFileLoad('./bot.js'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var router = express.Router()
+
+router.get('/', function (req, res) {
+  res.render(bot)
+});
+
+express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
