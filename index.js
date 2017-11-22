ed9ea61ee821e95ef9ed47876898d6c0f5cf5d7a
xrey007 <reinaldo.leal001@gmail.com>
2017-11-21 23:02:53 -0500

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
-var bot = require('./twitchActiveStreamers.js');
+var bot = require('./bot.js');
 
 var router = express.Router()
 
